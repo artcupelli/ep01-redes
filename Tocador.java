e1d095b510f16fdbc22e4ca5e3f14ea5fceefe06
Raquel Almeida <47835933+rakwtt@users.noreply.github.com>
2021-10-20 22:52:01 -0300

    atualizou de novo o tocador
    TODO: interromper a thread para pausar/parar

--- Tocador.java
+++ Tocador.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
@@ -11,15 +12,27 @@ public class Tocador {
     long clipPosicaoPausa;
 
     void tocaMusica(String caminho){
-
+        System.out.println("Entrei no tocaMusica :)");
+        
         try{
             File caminhoMusica = new File(caminho);
             
             if(caminhoMusica.exists()){
                 audioInput = AudioSystem.getAudioInputStream(caminhoMusica);
+
+                AudioFormat format = audioInput.getFormat();
+
+                // Calculando a duração da musica para setar o Thread.sleep()
+                long tamanhoAudio = caminhoMusica.length();
+                int tamanhoFrame = format.getFrameSize();
+                float frameRate = format.getFrameRate();
+                long duracaoEmMilissegundos = (long) (tamanhoAudio / (tamanhoFrame * frameRate)) * 1000;
+
                 clip = AudioSystem.getClip();
                 clip.open(audioInput);
                 clip.start();
+                clip.loop(-1);
+                Thread.sleep(duracaoEmMilissegundos);
             }else{
                 System.out.println("Poxa, não encontramos a música :(");
             }
@@ -40,5 +53,13 @@ public class Tocador {
     }
     
     void trocaMusica(){}
+
+    public static void main(String[] args) throws Exception{
+        Tocador tocador = new Tocador();
+
+        tocador.tocaMusica("[caminhodamusica].wav");
+
+        tocador.pausaMusica();
+    }
     
 }
