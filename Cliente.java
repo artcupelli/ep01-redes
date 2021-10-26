7629489c785335ebd83d5e0f27a93a69779a804c
Idemar <idemarneto13@gmail.com>
2021-10-26 00:03:11 -0300

    fila no servidor

--- Cliente.java
+++ Cliente.java
@@ -1,7 +1,5 @@
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -23,14 +21,51 @@ public class Cliente {
         Socket socket = new Socket(InetAddress.getLocalHost(), 6264);
         Menu menu = new Menu();
 
-        // Esse while não ta funfando direito, tem que estudar como faz
-        while(socket.getInetAddress().isReachable(500)){
-            System.out.print("conectado -\r");
-            System.out.print("conectado /\r");
-            System.out.print("conectado |\r");
-            System.out.print("conectado \\\r");
-            menu.menuComandos();
-        }
+            // reading the input from server
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // returning the output to the server : true statement is to flush the buffer
+            // otherwise
+            // we have to do it manuallyy
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            // taking the user input
+            Scanner scanner = new Scanner(System.in);
+            String userInput;
+            String response;
+            String clientName = "empty";
+
+            ClientRunnable clientRun = new ClientRunnable(socket);
+
+            new Thread(clientRun).start();
+            
+            // loop closes when user enters sair command
+            do {
+
+                if (clientName.equals("empty")) {
+                    System.out.print("\nColoque seu nome: ");
+                    userInput = scanner.nextLine();
+                    clientName = userInput;
+                    this.apelido = userInput;
+                    // output.println(userInput);
+                    if (userInput.equals("sair") || userInput.equals("SAIR")) {
+                        socket.close();
+                        break;
+                    }
+                } else {
+                    String message = ("(" + clientName + ")" + " message: ");
+                    System.out.println(message);
+                    userInput = scanner.nextLine();
+                    output.println("(" + clientName + ") " + userInput);
+                    output.println(userInput);
+                    if (userInput.equals("sair")) {
+                        // reading the input from server
+                        socket.close();
+                        break;
+                    }
+                }
+
+            } while (!userInput.equals("sair"));
 
     }
 
--- Fila.java
+++ Fila.java
@@ -7,11 +7,13 @@ public class Fila {
     private Map<Integer, Musica> musicasDisponiveis;
     // Fila para a reprodução
     private LinkedList<Musica> fila;
+    private ArrayList<ServerThread> threadList;
 
-    public Fila(){
+    public Fila(ArrayList<ServerThread> threadList){
 
         this.musicasDisponiveis = new HashMap<Integer, Musica>();
         this.fila = new LinkedList<Musica>();
+        this.threadList = threadList;
 
         File[] caminhoArq = new File("./musicas").listFiles();
         int j=1;
@@ -29,72 +31,74 @@ public class Fila {
     // TO-DO: Colocar quem colocou na fila
 
     //Mostra as músicas disponíveis para pedir
-    public void mostraMusicas(){
+    public void mostraMusicas(PrintWriter output){
         // Imprime a relação de número e música
-        System.out.println("MÚSICAS DISPONÍVEIS PARA REPRODUÇÃO - "+ this.musicasDisponiveis.size() +":");
+        for (ServerThread sT : threadList) sT.output.println("\nMÚSICAS DISPONÍVEIS PARA REPRODUÇÃO - "+ this.musicasDisponiveis.size() +":");
         for (int j = 1; this.musicasDisponiveis.containsKey(j); j++) {
-            System.out.println(this.musicasDisponiveis.get(j).getNumero() + ": " + this.musicasDisponiveis.get(j).getNome());
+            for (ServerThread sT : threadList) sT.output.println(this.musicasDisponiveis.get(j).getNumero() + ": " + this.musicasDisponiveis.get(j).getNome());
         }
-        System.out.println();
+        for (ServerThread sT : threadList) sT.output.println();
     }
 
     //Mostra as músicas presentes na fila
-    public void mostraFila(){
+    public void mostraFila(PrintWriter output){
         // Imprime a relação de número e música
-        if(this.fila.isEmpty()) System.out.println("*LISTA DE REPRODUÇÃO VAZIA*\n");
+        if(this.fila.isEmpty()) for (ServerThread sT : threadList) sT.output.println("\n*LISTA DE REPRODUÇÃO VAZIA*\n");
         else{
             Iterator<Musica> it = this.fila.iterator();
             int i = 1;
-            System.out.println("LISTA DE REPRODUÇÃO - "+ this.fila.size() +":");
+            for (ServerThread sT : threadList) sT.output.println("LISTA DE REPRODUÇÃO - "+ this.fila.size() +":");
             while(it.hasNext()){
                 Musica aux = it.next();
-                System.out.println(i + ": " + aux.getNome() + " (" + aux.getNumero() + ")");
+                for (ServerThread sT : threadList) sT.output.println(i + ": " + aux.getNome() + " (" + aux.getNumero() + ")");
                 i++;
             }
-            System.out.println();
+            for (ServerThread sT : threadList) sT.output.println();
         }
     }
 
     // Adiciona música no final da fila
-    public void adicionaMusica(int num){
+    public void adicionaMusica(PrintWriter output, int num){
         Musica novaMusica = this.musicasDisponiveis.get(num);
         if(novaMusica!=null){
             this.fila.addLast(novaMusica);
-            System.out.println(novaMusica.getNome() + " foi adicionado no fim da fila.\n"); 
+            for (ServerThread sT : threadList) sT.output.println("\n" + novaMusica.getNome() + " foi adicionado no fim da fila.\n"); 
         }
         else{
-            System.out.println("Número inválido para adição\n"); 
-            mostraMusicas();
+            for (ServerThread sT : threadList) sT.output.println("\nNúmero inválido para adição\n"); 
+            mostraMusicas(output);
         }
     }
 
     // Remove primeira incidêcia da música
-    public void removeMusica(int num){
+    public void removeMusica(PrintWriter output, int num){
         Musica novaMusica = this.musicasDisponiveis.get(num);
         int index = fila.indexOf(novaMusica);
         if (novaMusica != null && fila.contains(novaMusica)){
             this.fila.remove(novaMusica);
-            System.out.println(novaMusica.getNome() + " foi removida no índice " + (index+1) + "\n");
+            for (ServerThread sT : threadList) sT.output.println("\n" + novaMusica.getNome() + " foi removida no índice " + (index+1) + "\n");
         }
         else if(!fila.contains(novaMusica) && musicasDisponiveis.containsValue(novaMusica)){
-            System.out.println("Música não presente na fila\n");
-            mostraFila();
+            for (ServerThread sT : threadList) sT.output.println("\nMúsica não presente na fila\n");
+            mostraFila(output);
         }
         else{
-            System.out.println("Número inválido para remoção\n");
-            mostraMusicas();
+            for (ServerThread sT : threadList) sT.output.println("\nNúmero inválido para remoção\n");
+            mostraMusicas(output);
         }
     }
     // Vale a pena fazer um remover por indice
-    public void removeMusicaIndex(int num) {
+    public void removeMusicaIndex(PrintWriter output, int num) {
         int index = num-1;
         if(fila.get(index)!=null){
             Musica novaMusica = this.fila.remove(index);
-            System.out.println(novaMusica.getNome() + " foi removida na posição " + num + "\n");
+            for (ServerThread sT : threadList) sT.output.println("\n" + novaMusica.getNome() + " foi removida na posição " + num + "\n");
         }
         else{
-            System.out.println("Índice inválido\n");
-            mostraFila();
+            for (ServerThread sT : threadList){ 
+                sT.output.println("\nÍndice inválido\n");
+                mostraFila(output);
+            }
         }
     }
 
@@ -109,9 +113,9 @@ public class Fila {
         
     }
 
-    public void limparFila(){
+    public void limparFila(PrintWriter output){
         this.fila.clear();
-        mostraFila();
+        mostraFila(output);
     }
     
     public Map<Integer, Musica> getMusicasDisponiveis(){
